ff35fa290307dfad7be22153e9cca86c5f476e3e
임재원 <nemo0824@naver.com>
2025-12-31 16:08:10 +0900

    matzip02 feature: 아이콘, 절대경로

--- matzip/src/components/DrawerButton.tsx
+++ matzip/src/components/DrawerButton.tsx
@@ -1,19 +1,27 @@
-import { Text, Pressable } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
-import { MainDrawerPramList } from '../types/navigation';
+import { MainDrawerPramList } from '@/types/navigation';
+import Ionicons from '@react-native-vector-icons/ionicons';
+import { colors } from '@/constants/colors';
 
 type Navigation = DrawerNavigationProp<MainDrawerPramList>;
 
-const DrawerButton = () => {
+const DrawerButton = ({ color = colors.BLACK }) => {
   const navigation = useNavigation<Navigation>();
 
   return (
-    <Pressable onPress={() => navigation.openDrawer()}>
-      <Text style={{ fontSize: 16 }}>서랍</Text>
+    <Pressable onPress={() => navigation.openDrawer()} style={styles.container}>
+      <Ionicons name="menu" size={25} color={color} />
     </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+  },
+});
 
 export default DrawerButton;
